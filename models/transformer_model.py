371a85ce06dc7f20f7b97008a23448fa529d4335
Kody James <kodyj123@gmail.com>
2025-05-21 22:00:30 -0400

    Update imports

--- models/transformer_model.py
+++ models/transformer_model.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import random
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from models.base_model import BaseLanguageModel
